a74e405b5d90c52bcf6ba74265b7fe7c8e67e189
YBDmatS <sasia.mathilde@gmail.com>
2026-02-25 15:41:07 +0100

    [REFACTOR] Step 3 - Update routing country/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: DashboardComponent,
   },
   {
-    path : 'country/:countryName',
+    path : 'country/:id',
     component : CountryDetailComponent
   },
 
