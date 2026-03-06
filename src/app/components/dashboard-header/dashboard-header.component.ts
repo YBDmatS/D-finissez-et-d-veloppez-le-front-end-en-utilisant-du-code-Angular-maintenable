28f31c3295b9eaeac060cbcd0f13f795c9004966
YBDmatS <sasia.mathilde@gmail.com>
2026-03-06 16:04:00 +0100

    [REFACTOR] Step 2.1 – Improve dashboard header responsive design

--- src/app/components/dashboard-header/dashboard-header.component.ts
+++ src/app/components/dashboard-header/dashboard-header.component.ts
@@ -2,11 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Kpi } from 'src/app/models/view-models/kpi.model';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  selector: 'app-dashboard-header',
+  templateUrl: './dashboard-header.component.html',
+  styleUrls: ['./dashboard-header.component.scss'],
 })
-export class HeaderComponent {
+export class DashboardHeaderComponent {
   @Input() public titlePage!: string;
   @Input() public kpis!: Kpi[];
 }
