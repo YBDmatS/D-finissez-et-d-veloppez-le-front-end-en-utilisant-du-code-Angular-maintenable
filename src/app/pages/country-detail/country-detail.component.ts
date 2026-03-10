82960d38c3ce3512b55900bb25beee91077c76b0
YBDmatS <sasia.mathilde@gmail.com>
2026-03-10 12:11:21 +0100

    [REFACTOR] Step 2.2 - Country detail observable flow with page view model and async pipe

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,9 +1,7 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { LineChartComponent } from 'src/app/components/charts/line-chart/line-chart.component';
-import { CountryMedalsByYear } from 'src/app/models/view-models/CountryMedalsByYear.vm';
-import { Kpi } from 'src/app/models/view-models/kpi.vm';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CountryDetailPageVm } from 'src/app/models/view-models/CountryDetail-page.vm';
 import { OlympicService } from 'src/app/services/olympic.service';
 
 @Component({
@@ -11,41 +9,9 @@ import { OlympicService } from 'src/app/services/olympic.service';
   templateUrl: './country-detail.component.html',
   styleUrls: ['./country-detail.component.scss'],
 })
-export class CountryDetailComponent implements OnInit {
-  public titlePage = 'Aucun pays sélectionné';
-  public kpis!: Kpi[];
-  public countryMedalsByYears!: CountryMedalsByYear[];
-  public error!: string;
-
-  private readonly route = inject(ActivatedRoute);
-  private readonly router = inject(Router);
+export class CountryDetailComponent {
   private readonly olympicService = inject(OlympicService);
-
-  @ViewChild(LineChartComponent)
-  lineChartComponent!: LineChartComponent;
-
-  ngOnInit() {
-    const id = Number(this.route.snapshot.paramMap.get('id') || '');
-
-    this.olympicService.getCountryDetailById(id).subscribe({
-      next: (country) => {
-        if (!country) {
-          this.router.navigate(['/']);
-          return;
-        }
-        this.titlePage = country.country;
-      },
-    });
-
-    this.olympicService.getCountryKpis(id).subscribe({
-      next: (kpis) => (this.kpis = kpis),
-      error: (error: HttpErrorResponse) => (this.error = error.message),
-    });
-
-    this.olympicService.getCountryMedalsByYear(id).subscribe({
-      next: (data) => {
-        this.countryMedalsByYears = data;
-      },
-    });
-  }
+  private readonly route = inject(ActivatedRoute);
+  private readonly id: number = Number(this.route.snapshot.paramMap.get('id'));
+  public vm$: Observable<CountryDetailPageVm> = this.olympicService.getCountryDetailPageVm(this.id);
 }
