e8fd23ae5ab94c1e29459fa09cd12599a153bcc3
YBDmatS <sasia.mathilde@gmail.com>
2026-03-10 12:25:57 +0100

    [FEAT] Step 2.2 - Improve country detail page responsiveness, enhance back button UI and add Material icon

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -12,6 +13,11 @@ import { OlympicService } from 'src/app/services/olympic.service';
 export class CountryDetailComponent {
   private readonly olympicService = inject(OlympicService);
   private readonly route = inject(ActivatedRoute);
+  private readonly location = inject(Location);
   private readonly id: number = Number(this.route.snapshot.paramMap.get('id'));
   public vm$: Observable<CountryDetailPageVm> = this.olympicService.getCountryDetailPageVm(this.id);
+
+  public goBack(): void {
+    this.location.back();
+  }
 }
